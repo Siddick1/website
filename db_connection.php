8d17bd58b5c8490293fd2b78f6bffd3f99991903
Siddick1 <55353396+Siddick1@users.noreply.github.com>
2020-05-16 05:01:44 +0200

    update

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','heroku_7e2753778461989');
+define('DB_USER','baa4b258b57845');
+define('DB_PASS' ,'640ff255');
 define('DB_NAME', 'nagriblog');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 if (mysqli_connect_errno())
